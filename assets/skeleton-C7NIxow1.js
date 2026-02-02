016c9b2126d0105dd32ac210f7de7d92d3a084c7
Guilherme <glh.henrique@gmail.com>
2026-02-02 21:28:26 +0000

    Updates

---
+++ assets/skeleton-C7NIxow1.js
@@ -0,0 +1,1 @@
+import{j as e}from"./vendor-RJ-jBvc8.js";import{c as t}from"./index-Bu10ICyF.js";function o({className:r}){return e.jsx("div",{className:t("animate-pulse rounded-md bg-neutral-200 dark:bg-neutral-800",r)})}export{o as S};
