66db5a8852755d397a85f98c849a088f456041be
Guilherme <glh.henrique@gmail.com>
2026-02-09 21:46:37 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/only-training/sw.js', { scope: '/only-training/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
