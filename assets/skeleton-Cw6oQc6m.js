12d50a05ee0262ad6cfdf4c3c17f5a383d173a96
glh-henrique <glh-henrique@users.noreply.github.com>
2026-07-01 21:15:29 +0000

    deploy: e3861e0870bf9e1be7285549cbcafcdeb82ac35d

---
+++ assets/skeleton-Cw6oQc6m.js
@@ -0,0 +1,1 @@
+import{j as r}from"./vendor-DPFdScdP.js";import{e as t}from"./index-B40lQJOw.js";function o({className:e}){return r.jsx("div",{className:t("animate-pulse rounded-md bg-neutral-200 dark:bg-neutral-800",e)})}export{o as S};
